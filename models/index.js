c104c739692971a46208108308a17c7cb2ac2f8f
quiet-honey <skap0329@naver.com>
2024-07-26 22:56:46 +0900

    scaffolding friend API implementation

--- models/index.js
+++ models/index.js
@@ -1,17 +1,22 @@
-const Sequelize = require('sequelize');
-const User = require('./user');
-const env = process.env.NODE_ENV || 'team5-db';
-const config = require('../config/config')[env];
+const Sequelize = require("sequelize");
+const User = require("./user");
+const Friend = require("./friend");
+const env = process.env.NODE_ENV || "team5-db";
+const config = require("../config/config")[env];
 
 const db = {};
 const sequelize = new Sequelize(
-    config.database, config.username, config.password, config,
+    config.database,
+    config.username,
+    config.password,
+    config
 );
 
 db.sequelize = sequelize;
 db.User = User;
 
 User.initiate(sequelize);
+Friend.initiate(sequelize);
 
 User.associate(db);
 

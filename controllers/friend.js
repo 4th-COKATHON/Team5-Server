c681ed572d82335a291051ac51478bc269c6ece0
quiet-honey <skap0329@naver.com>
2024-07-26 23:21:09 +0900

    temp

---
+++ controllers/friend.js
@@ -0,0 +1,19 @@
+exports.createFriend = async (req, res, next) => {
+  const { userId, friendId, status } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.redirect("/join?error=exist");
+    }
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email,
+      nick,
+      password: hash,
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
